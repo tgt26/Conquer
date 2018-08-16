2af37b8687b6e1a18ac9c7e6732728bfd8178ebb
tgt26 <38292380+tgt26@users.noreply.github.com>
2018-08-16 18:07:32 -0400

    Initial commit

---
+++ script/main.js
@@ -0,0 +1,9 @@
+// Respnosive NavBar
+
+function openNav() {
+  document.getElementById("mySideBar").style.width = "200px";
+}
+
+function closeNav() {
+  document.getElementById("mySideBar").style.width = "0";
+}
